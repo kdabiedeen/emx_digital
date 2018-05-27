c947ff2b9f999b046f3f9fa0253d3ddcb54d410c
Kevin Dabiedeen <kdabiedeen@Kevins-MacBook-Pro.local>
2018-05-27 16:44:32 -0400

    built responses

--- routes/resume.js
+++ routes/resume.js
@@ -3,16 +3,47 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  if(req.query.q == "Ping")
-    res.send("OK")
-  else if(req.query.q == "Name")
-    res.send("Kevin Dabiedeen")
-  else
-    res.send("Monster")
-});
-
-router.post('/', function(req, res, next){
-  res.send("OK")
+  var url_param = req.query.q;
+  var response;
+  switch(url_param){
+    case "Ping":
+      response = "OK";
+      break;
+    case "Name":
+      response = "Kevin Dabiedeen"
+      break;
+    case "Phone":
+      response = "646-508-8131"
+      break;
+    case "Email Address":
+      response = "kdabiedeen1@gmail.com"
+      break;
+    case "Degree":
+      response = "B.S. Computer Science"
+      break;
+    case "Position":
+      response = "Software Engineer"
+      break;
+    case "Years":
+      response = "1 - 2 Years"
+      break;
+    case "Status":
+      response = "Yes"
+      break;
+    case "Resume":
+      response = "https://linkedin.com/in/kdabiedeen"
+      break;
+    case "Source":
+      response = "https://github.com/kdabiedeen/emx_digital"
+      break;
+    case "Referrer":
+      response = "Jenny"
+      break;
+    case "Puzzle":
+      response = "Puzzle"
+      break;
+    res.send(response)
+  }
 });
 
 module.exports = router;
