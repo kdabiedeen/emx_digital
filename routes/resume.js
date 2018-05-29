37d7474bb16b8245fa1b2c4b1077d32a85be0593
Kevin Dabiedeen <kdabiedeen@Kevins-MacBook-Pro.local>
2018-05-29 03:32:32 -0400

    test

--- routes/resume.js
+++ routes/resume.js
@@ -5,7 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   var call_result = function(input){
     var rows = input.split(/\r?\n/);
-    return "what" + rows.length;
+    return rows.join("*9*");
     var indexOfEquals = -1;
 
     var A = ['-', '-', '-', '-'];
